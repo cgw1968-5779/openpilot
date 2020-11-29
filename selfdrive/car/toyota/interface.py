16bbf8b6ffdda2e6f0b3654c8aa3f9baa18a76d5
marc88chen <66193562+marc88chen@users.noreply.github.com>
2020-11-30 00:08:24 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -259,8 +259,8 @@ class CarInterface(CarInterfaceBase):
         tire_stiffness_factor = 0.996  # not optimized yet
         ret.lateralTuning.init('indi')
         ret.lateralTuning.indi.innerLoopGain = 23
-        ret.lateralTuning.indi.outerLoopGainBP = [19, 21, 23, 25]
-        ret.lateralTuning.indi.outerLoopGainV = [17.5, 19.9, 21.99, 22.99]
+        ret.lateralTuning.indi.outerLoopGainBP = [18, 19, 21, 22]
+        ret.lateralTuning.indi.outerLoopGainV = [16, 19, 21, 22.99]
         ret.lateralTuning.indi.timeConstant = 5.5
         ret.lateralTuning.indi.actuatorEffectiveness = 23
       else:
