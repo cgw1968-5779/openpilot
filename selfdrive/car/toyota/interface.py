e5b9bb5a4328584b6906b5f1e34ad2059287f644
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-01-03 17:27:54 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -224,14 +224,14 @@ class CarInterface(CarInterfaceBase):
       ret.steerActuatorDelay = 0.45
       ret.steerLimitTimer = 5.0
       ret.lateralTuning.init('indi')
-      ret.lateralTuning.indi.innerLoopGainBP = [16.7, 25, 33.3, 36.1]
-      ret.lateralTuning.indi.innerLoopGainV = [15, 15, 15, 15]
-      ret.lateralTuning.indi.outerLoopGainBP = [16.7, 25, 33.3, 36.1]
-      ret.lateralTuning.indi.outerLoopGainV = [9, 13.8, 14.99, 18]
-      ret.lateralTuning.indi.timeConstantBP = [13.8, 13.81, 16.7, 16.71, 22, 22.01]
-      ret.lateralTuning.indi.timeConstantV = [0.5, 1, 1, 2, 2, 3]
-      ret.lateralTuning.indi.actuatorEffectivenessBP = [16.7, 25, 33.3, 36.1]
-      ret.lateralTuning.indi.actuatorEffectivenessV = [15, 15, 15, 15]
+      ret.lateralTuning.indi.innerLoopGainBP = [16.7, 25, 36.1]
+      ret.lateralTuning.indi.innerLoopGainV = [15, 15, 15]
+      ret.lateralTuning.indi.outerLoopGainBP = [16.7, 25, 36.1]
+      ret.lateralTuning.indi.outerLoopGainV = [9, 14.99, 14.99]
+      ret.lateralTuning.indi.timeConstantBP = [16.7, 16.71, 22, 22.01, 26, 26.01, 30.55, 30.56, 36, 36.01]
+      ret.lateralTuning.indi.timeConstantV = [0.5, 1, 1, 2, 2, 3, 3, 4, 4, 5]
+      ret.lateralTuning.indi.actuatorEffectivenessBP = [16.7, 25, 36.1]
+      ret.lateralTuning.indi.actuatorEffectivenessV = [15, 15, 15]
 
     elif candidate == CAR.RAV4H_TSS2:
       stop_and_go = True
