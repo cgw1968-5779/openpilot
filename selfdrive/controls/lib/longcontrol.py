832d4cc8b9e7da8b1c20ae115ff3153c57c3d39f
marc88chen <66193562+marc88chen@users.noreply.github.com>
2021-02-15 08:02:46 +0800

    Update longcontrol.py

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -8,11 +8,11 @@ LongCtrlState = log.ControlsState.LongControlState
 
 STOPPING_EGO_SPEED = 0.5
 STOPPING_TARGET_SPEED_OFFSET = 0.01
-STARTING_TARGET_SPEED = 0.75
+STARTING_TARGET_SPEED = 0.6
 BRAKE_THRESHOLD_TO_PID = 0.2
 
 STOPPING_BRAKE_RATE = 0.15  # brake_travel/s while trying to stop
-BRAKE_STOPPING_TARGET = 0.695  # apply at least this amount of brake to maintain the vehicle stationary
+BRAKE_STOPPING_TARGET = 0.7  # apply at least this amount of brake to maintain the vehicle stationary
 
 RATE = 100.0
 
