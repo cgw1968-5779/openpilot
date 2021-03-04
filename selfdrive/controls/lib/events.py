0bc1d7fe8921a6a37ba7f921d7e65b665eb031ad
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-03-05 00:56:54 +0800

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -229,7 +229,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
   EventName.startup: {
     ET.PERMANENT: Alert(
       _("Be ready to take over at any time"),
-      _("Do not forget to PRAY!"),
+      _("Arne Dragonpilot Ultimate Version!"),
       AlertStatus.normal, AlertSize.mid,
       Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0., 6.),
   },
