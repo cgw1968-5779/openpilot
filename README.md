e940b096432581d2f45814efc381c2095f78d1aa
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2021-04-13 11:58:47 -0700

    SYNC: master and faster ci. fe2f63849ae467fee70c72d18fa52a184c056331

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# IF YOU AREN'T KUMAR, ARNE OR STUPE THIS VERSION ISN'T FOR YOU
+# Can someone make this look nice.
 
 
 
-ALPHA VERSION. DO NOT USE. LOGGING IS STILL ON.
+ALPHA VERSION. DO NOT USE. LOGGING IS STILL off.
 
 New ArnePilot based off master which means if everythings goes to as we planned we should be uptodated all the time.
-
-This version isn't for the public and is WHITELISTED
--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -29,8 +29,6 @@ ArizonaMode = op_params.get('ArizonaMode')
 
 FW_SIGNATURE = get_expected_signature()
 
-DISABLE_LTE_ONROAD = os.path.exists("/persist/disable_lte_onroad") or TICI
-
 ThermalStatus = log.DeviceState.ThermalStatus
 NetworkType = log.DeviceState.NetworkType
 NetworkStrength = log.DeviceState.NetworkStrength
@@ -355,12 +353,14 @@ def thermald_thread():
 
     # Handle offroad/onroad transition
     should_start = all(startup_conditions.values())
-    if should_start:
-      if not should_start_prev:
-        params.delete("IsOffroad")
-        if TICI and DISABLE_LTE_ONROAD:
-          os.system("sudo systemctl stop --no-block lte")
+    if should_start != should_start_prev or (count == 0):
+      params.put_bool("IsOffroad", not should_start)
+      HARDWARE.set_power_save(not should_start)
+      if TICI:
+        fxn = "stop" if should_start else "start"
+        os.system(f"sudo systemctl {fxn} --no-block lte")
 
+    if should_start:
       off_ts = None
       if started_ts is None:
         started_ts = sec_since_boot()
@@ -368,11 +368,6 @@ def thermald_thread():
     else:
       if startup_conditions["ignition"] and (startup_conditions != startup_conditions_prev):
         cloudlog.event("Startup blocked", startup_conditions=startup_conditions)
-
-      if should_start_prev or (count == 0):
-        params.put_bool("IsOffroad", True)
-        if TICI and DISABLE_LTE_ONROAD:
-          os.system("sudo systemctl start --no-block lte")
 
       started_ts = None
       if off_ts is None:
