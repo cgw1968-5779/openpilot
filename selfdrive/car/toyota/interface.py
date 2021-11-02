b8daa1fec1711734fef8a237aaf85c5be6382559
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-11-02 12:30:09 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -299,13 +299,13 @@ class CarInterface(CarInterfaceBase):
       ret.steerLimitTimer = 1
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGainBP = [8.3, 8.31, 19.4]
-      ret.lateralTuning.indi.innerLoopGainV = [2.9, 9, 15]
+      ret.lateralTuning.indi.innerLoopGainV = [3.9, 9, 15]
       ret.lateralTuning.indi.outerLoopGainBP = [8.3, 8.31, 16.7]
       ret.lateralTuning.indi.outerLoopGainV = [2.2, 7.75, 15]
       ret.lateralTuning.indi.timeConstantBP = [10, 20, 24]
       ret.lateralTuning.indi.timeConstantV = [0.85, 1.4, 2.2]
       ret.lateralTuning.indi.actuatorEffectivenessBP = [8.3, 8.31, 19.4]
-      ret.lateralTuning.indi.actuatorEffectivenessV = [2.9, 9, 15]
+      ret.lateralTuning.indi.actuatorEffectivenessV = [3.9, 9, 15]
 
       # 2019+ Rav4 TSS2 uses two different steering racks and specific tuning seems to be necessary.
       # See https://github.com/commaai/openpilot/pull/21429#issuecomment-873652891
@@ -596,9 +596,9 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.deadzoneBP = [0., 9]
       ret.longitudinalTuning.deadzoneV = [.0, .15]
       ret.longitudinalTuning.kpBP = [0., 5., 10., 35.]
-      ret.longitudinalTuning.kpV = [3.0, 3.0, 2.4, 1.5]
+      ret.longitudinalTuning.kpV = [3.3, 2.4, 2.4, 1.5]
       ret.longitudinalTuning.kiBP = [0., 5., 13.9, 25., 35.]
-      ret.longitudinalTuning.kiV = [.3, .31, .32, .33, .34]
+      ret.longitudinalTuning.kiV = [.28, .31, .34, .33, .34]
       ret.stoppingDecelRate = 0.06  # reach stopping target smoothly
       ret.startingAccelRate = 8.0  # release brakes fast
       ret.startAccel = 14.0  # Accelerate from 0 faster
