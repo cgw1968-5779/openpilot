eb5d6fba11b90684324c320750b05819881bb295
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-11-05 19:34:31 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -596,9 +596,9 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.deadzoneBP = [0., 9]
       ret.longitudinalTuning.deadzoneV = [.0, .15]
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
-      ret.longitudinalTuning.kpV = [3.7, 2.5, 1.6]
+      ret.longitudinalTuning.kpV = [3.4, 2.3, 1.5]
       ret.longitudinalTuning.kiBP = [0., 5., 13.9, 25., 35.]
-      ret.longitudinalTuning.kiV = [.33, .31, .32, .33, .34]
+      ret.longitudinalTuning.kiV = [.3, .31, .32, .33, .34]
       ret.stoppingDecelRate = 0.06  # reach stopping target smoothly
       ret.startingAccelRate = 8.0  # release brakes fast
       ret.startAccel = 14.0  # Accelerate from 0 faster
