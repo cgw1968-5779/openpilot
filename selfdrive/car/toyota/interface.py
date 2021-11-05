d9b863af7dde87c06c977e9e99cf0eb86aa75795
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-11-05 23:25:12 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -303,9 +303,9 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.indi.outerLoopGainBP = [8.3, 8.31, 16.7]
       ret.lateralTuning.indi.outerLoopGainV = [3.2, 8.75, 15]
       ret.lateralTuning.indi.timeConstantBP = [10, 17, 24]
-      ret.lateralTuning.indi.timeConstantV = [0.75, 1.2, 2.2]
+      ret.lateralTuning.indi.timeConstantV = [0.75, 1.0, 2.2]
       ret.lateralTuning.indi.actuatorEffectivenessBP = [8.3, 8.31, 19.4]
-      ret.lateralTuning.indi.actuatorEffectivenessV = [3.9, 9, 15]
+      ret.lateralTuning.indi.actuatorEffectivenessV = [3.2, 10, 15]
 
       # 2019+ Rav4 TSS2 uses two different steering racks and specific tuning seems to be necessary.
       # See https://github.com/commaai/openpilot/pull/21429#issuecomment-873652891
