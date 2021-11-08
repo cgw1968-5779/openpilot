35a697a156f9295c1b619aecf92dd3b5aa5f5ec1
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-11-08 09:22:15 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -56,7 +56,7 @@ _DP_CRUISE_MIN_BP = [0.0, 3.0, 5.0, 20.0, 33.3, 40.0, 55.0]
 _DP_CRUISE_MAX_V = [3.3, 3.0, 1.8, 1.0, .7, .3, .1]
 _DP_CRUISE_MAX_V_ECO = [3.0, 3.0, 1.6, .9, .55, .25, .08]
 _DP_CRUISE_MAX_V_SPORT = [3.5, 3.5, 3.0, 2.5, 2.5, 3.5, 3.5]
-_DP_CRUISE_MAX_BP = [0., 6., 8., 11., 15., 20., 55.]
+_DP_CRUISE_MAX_BP = [0., 4., 8., 11., 15., 20., 55.]
 
 def dp_calc_cruise_accel_limits(v_ego, dp_profile):
   if dp_profile == DP_ACCEL_ECO:
