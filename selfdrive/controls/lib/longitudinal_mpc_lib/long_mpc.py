e02371dbbffa61e14a1e0447fa374007139048ee
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2022-02-06 19:26:30 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -340,7 +340,7 @@ class LongitudinalMpc():
       # At slow speeds more time, decrease time up to 60mph
       # in kph ~= 0    10    20     30     40     50     60     90    150
       x_vel = [0,   2.788,  5.56,  8.333,  11.11, 13.89, 16.67, 25.0, 41.67]
-      y_dist = [1.24, 1.24, 1.27,  1.29,   1.35,  1.35,   1.35,  1.1,  1.3]
+      y_dist = [1.24, 1.24, 1.27,  1.29,   1.34,  1.34,   1.3,  1.1,  1.3]
       self.desired_TF = np.interp(carstate.vEgo, x_vel, y_dist)
     elif carstate.distanceLines == 2: # Relaxed
       x_vel = [0.0, 2.788,  5.56,  8.333,  11.11, 13.89, 19.44, 27.78, 41.67]  # velocities
@@ -349,7 +349,7 @@ class LongitudinalMpc():
       #self.desired_TF = 1.7
     else:
       x_vel = [0.0, 2.788,  5.56,  8.333,  11.11, 13.89, 19.44, 27.78, 41.67]  # velocities
-      y_dist = [1.37, 1.37, 1.37,  1.4,   1.47,  1.47,  1.698, 1.98,  2.2]
+      y_dist = [1.37, 1.37, 1.37,  1.4,   1.47,  1.47,  1.698, 1.98,  2.3]
       self.desired_TF = np.interp(carstate.vEgo, x_vel, y_dist)
       #self.desired_TF = T_FOLLOW
 
