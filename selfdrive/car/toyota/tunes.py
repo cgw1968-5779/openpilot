f9e591bffe3005982b74ea21b7010ec1ba669f46
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2022-02-07 23:21:48 +0800

    Update tunes.py

--- selfdrive/car/toyota/tunes.py
+++ selfdrive/car/toyota/tunes.py
@@ -75,14 +75,14 @@ def set_lat_tune(tune, name):
 
   elif name == LatTunes.INDI_RAV4_TSS2:
     tune.init('indi')
-    tune.indi.innerLoopGainBP = [20, 24, 30]
-    tune.indi.innerLoopGainV = [7.25, 7.5, 9]
-    tune.indi.outerLoopGainBP = [20, 24, 30]
-    tune.indi.outerLoopGainV = [6, 7.25, 6]
-    tune.indi.timeConstantBP = [20, 24]
-    tune.indi.timeConstantV = [2.0, 2.2]
-    tune.indi.actuatorEffectivenessBP = [20, 24]
-    tune.indi.actuatorEffectivenessV = [2, 3]
+    tune.indi.innerLoopGainBP = [8, 10, 16.71, 20, 30]
+    tune.indi.innerLoopGainV = [10, 11.6, 13, 15, 15]
+    tune.indi.outerLoopGainBP = [8, 10, 16.71, 20, 30]
+    tune.indi.outerLoopGainV = [7., 11.5, 12.8, 14.7, 14.6
+    tune.indi.timeConstantBP = [4.15, 8.3, 11.1, 13.9, 16.7, 20, 25, 30]
+    tune.indi.timeConstantV = [0.1, 0.2, 0.3, 0.4, 0.5, 0.8, 2.0, 2.8]
+    tune.indi.actuatorEffectivenessBP = [1, 8, 16.7, 16.71, 20]
+    tune.indi.actuatorEffectivenessV = [1, 2, 3, 3, 3]
 
   elif name == LatTunes.LQR_RAV4:
     tune.init('lqr')
