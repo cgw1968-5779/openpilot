b027ace37a9193b397becc8219c43eda73a4e840
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2022-02-12 00:24:30 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -49,7 +49,7 @@ T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N+1) for idx in range(N
 T_IDXS = np.array(T_IDXS_LST)
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
-T_FOLLOW = 1.8
+T_FOLLOW = 1.45
 COMFORT_BRAKE = 2.5
 STOP_DISTANCE = 5.5
 
@@ -246,7 +246,7 @@ class LongitudinalMpc():
   def get_cost_multipliers(self):
     v_ego = self.x0[1]
     v_ego_bps = [0, 10]
-    TFs = [1.0, 1.5, T_FOLLOW]
+    TFs = [1.0, 1.25, T_FOLLOW]
     # KRKeegan adjustments to costs for different TFs
     # these were calculated using the test_longitudial.py deceleration tests
     x_ego_tf = interp(self.desired_TF, TFs, [2., 1.3, 1.])
