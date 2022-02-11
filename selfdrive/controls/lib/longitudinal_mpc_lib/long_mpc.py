1f8ebb0c6003478d78fcfa8d7bc42cc0308f4498
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2022-02-11 09:13:51 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -51,7 +51,7 @@ T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
 T_FOLLOW = 1.8
 COMFORT_BRAKE = 1.5
-STOP_DISTANCE = 5.0
+STOP_DISTANCE = 5.5
 
 def get_stopped_equivalence_factor(v_lead, v_ego, t_follow=T_FOLLOW):
   # KRKeegan this offset rapidly decreases the following distance when the lead pulls
