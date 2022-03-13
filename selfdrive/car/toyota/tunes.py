fb53072879527547fb9ec9b4c27710f9113adae0
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2022-03-13 22:16:17 +0800

    Update tunes.py

--- selfdrive/car/toyota/tunes.py
+++ selfdrive/car/toyota/tunes.py
@@ -87,7 +87,7 @@ def set_lat_tune(tune, name):
     #tune.indi.outerLoopGainV = [3.15, 5.2, 5.85, 8.47, 12.32, 12.95, 14.98]
     tune.indi.timeConstantBP = [1, 15, 20, 30]
     tune.indi.timeConstantV = [0.1, 0.43, 0.8, 1.6]
-    tune.indi.actuatorEffectivenessBP = [1, 16.7, 21, 36]
+    tune.indi.actuatorEffectivenessBP = [1, 16.7, 25, 36]
     tune.indi.actuatorEffectivenessV = [11, 15, 15, 7.5]
 
   elif name == LatTunes.LQR_RAV4:
