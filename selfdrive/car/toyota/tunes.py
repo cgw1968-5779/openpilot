b49d17234f9952afd3289e7ec2fe375df8b99190
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2022-03-19 20:31:24 +0800

    Update tunes.py

--- selfdrive/car/toyota/tunes.py
+++ selfdrive/car/toyota/tunes.py
@@ -79,8 +79,8 @@ def set_lat_tune(tune, name):
     tune.indi.innerLoopGainV = [5.2,    7.8,    8.8,   10,    12.8,  15]
     tune.indi.outerLoopGainBP = [8.3,   12.10,  13.9,  16.7,  20,    30]
     tune.indi.outerLoopGainV = [4.955,  7.6857, 8.603, 9.865, 12.78, 14.999]
-    tune.indi.timeConstantBP = [8.32, 8.33, 13.9, 18,  20,  30]
-    tune.indi.timeConstantV = [0.2, 0.2425, 0.4, 0,57, 1.0, 1.5]
+    tune.indi.timeConstantBP = [8.32, 8.33, 13.9, 18,  20,  23.88, 23.89,  30]
+    tune.indi.timeConstantV = [0.2, 0.2425, 0.4, 0,57, 0.8, 0.9, 1.2, 1.5]
     tune.indi.actuatorEffectivenessBP = [10, 11, 11.1, 25, 33]
     tune.indi.actuatorEffectivenessV = [8, 12, 15, 15, 15]
 
