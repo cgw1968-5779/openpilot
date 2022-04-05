427e5b979bed5d6b8daff53a87223cdf0db63116
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2022-04-05 22:28:51 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -127,6 +127,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 14.3
       tire_stiffness_factor = 0.7933
       ret.mass = 3585. * CV.LB_TO_KG + STD_CARGO_KG  # Average between ICE and Hybrid
+      set_lat_tune(ret.lateralTuning, LatTunes.INDI_RAV4_TSS2)
       ret.steerActuatorDelay = 0
       ret.steerRateCost = 1.3
       ret.steerLimitTimer = 0.35
