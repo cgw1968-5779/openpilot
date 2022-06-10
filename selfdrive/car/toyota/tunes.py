bb78b1c3eebfac40db2e115291fd83d0366784e2
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2022-06-10 20:29:00 +0800

    Update tunes.py

--- selfdrive/car/toyota/tunes.py
+++ selfdrive/car/toyota/tunes.py
@@ -62,7 +62,7 @@ def set_lat_tune(tune, name, MAX_TORQUE=2.5, FRICTION=0.06):
     tune.torque.useSteeringAngle = True
     tune.torque.kp = 1.7/ MAX_TORQUE
     tune.torque.kf = 1.4 / MAX_TORQUE
-    tune.torque.ki = 0.4 / MAX_TORQUE
+    tune.torque.ki = 0.5 / MAX_TORQUE
     tune.torque.friction = FRICTION
 
   elif name == LatTunes.INDI_PRIUS:
