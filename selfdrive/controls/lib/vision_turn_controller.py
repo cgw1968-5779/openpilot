1b8c4e426fd357c7adb90053f8c790720ea61911
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2022-07-29 12:57:07 +0800

    Update vision_turn_controller.py

--- selfdrive/controls/lib/vision_turn_controller.py
+++ selfdrive/controls/lib/vision_turn_controller.py
@@ -35,8 +35,8 @@ _ENTERING_SMOOTH_DECEL_BP = [1.3, 3.]  # absolute value of lat acc ahead
 
 # Lookup table for the acceleration for the TURNING state
 # depending on the current lateral acceleration of the vehicle.
-_TURNING_ACC_V = [0.5, 0., -0.4]  # acc value
-_TURNING_ACC_BP = [1.5, 2.3, 3.]  # absolute value of current lat acc
+_TURNING_ACC_V = [1.1, 0.96, -0.1]  # acc value
+_TURNING_ACC_BP = [1.5, 8, 10.0]  # absolute value of current lat acc
 
 _LEAVING_ACC = 0.5  # Confortble acceleration to regain speed while leaving a turn.
 
