0a8c88fb6994b02b27b2ef601f3304825def6d27
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2023-04-25 17:17:01 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -247,12 +247,12 @@ class LongitudinalPlanner:
       return desired_tf
     if self.dp_following_profile_ctrl:
       if self.dp_following_profile == 0:
-        x_vel =  [1.1,  3.3,  5.5,    13.89,  19.7,   25.0,   41.67]
-        y_dist = [1.0,  1.2,  1.3,    1.34,    1.34,   1.23,   1.34]
+        x_vel =  [1.1,  5.5,   13.89,  19.7,   25.0,   41.67]
+        y_dist = [1.0,  1.3,   1.34,    1.34,   1.23,   1.34]
         desired_tf = np.interp(v_ego, x_vel, y_dist)
       elif self.dp_following_profile == 1:
-        x_vel =  [5.556, 19.7,   41.67]
-        y_dist = [1.4,   1.5,    1.6 ]
+        x_vel =  [1.1,  5.556, 19.7,   41.67]
+        y_dist = [1.0,  1.4,   1.5,    1.6 ]
         desired_tf = np.interp(v_ego, x_vel, y_dist)
       elif self.dp_following_profile == 2:
         x_vel =  [1.1,  8.3,   19.7,   41.67]
