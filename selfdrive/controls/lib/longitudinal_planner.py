608754a0b146c03896d21f95b121e31aed4c0c50
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2023-06-01 11:44:32 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -41,9 +41,9 @@ DP_ACCEL_NORMAL = 1
 DP_ACCEL_SPORT = 2
 
 # accel profile by @arne182 modified by cgw
-_DP_CRUISE_MIN_V =       [-0.55, -0.65, -0.70, -0.70, -0.65,  -0.45]
-_DP_CRUISE_MIN_V_ECO =   [-0.50, -0.60, -0.60, -0.68, -0.60,  -0.40]
-_DP_CRUISE_MIN_V_SPORT = [-1.0,  -1.0,  -1.0,  -0.90, -0.80,  -0.7]
+_DP_CRUISE_MIN_V =       [-0.55, -0.65, -0.70, -0.65, -0.55,  -0.35]
+_DP_CRUISE_MIN_V_ECO =   [-0.50, -0.60, -0.60, -0.60, -0.50,  -0.30]
+_DP_CRUISE_MIN_V_SPORT = [-0.70,  -0.75, -0.75, -0.75, -0.65,  -0.45]
 _DP_CRUISE_MIN_BP =      [0.,    8.3,   14,    20.,   30.,   55.]
 
 _DP_CRUISE_MAX_V =       [3.5, 3.5, 2.0, 1.5, 1.06, 0.90, 0.68, 0.45, 0.34, 0.13]
