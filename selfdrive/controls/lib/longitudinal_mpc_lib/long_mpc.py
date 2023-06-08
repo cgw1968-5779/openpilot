907bc044ee2d3f944025c01d440f90f8524fe9f3
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2023-06-09 00:21:54 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -54,7 +54,7 @@ T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
 MAX_ACCEL = 2.0
 T_FOLLOW = 1.45
-COMFORT_BRAKE = 2.5
+COMFORT_BRAKE = 2.6
 STOP_DISTANCE = 5.3
 
 def get_stopped_equivalence_factor(v_lead, v_ego):
