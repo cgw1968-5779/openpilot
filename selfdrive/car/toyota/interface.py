007be61bf6e5b16299001cafed7d1c9b3d773536
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2023-09-16 14:33:03 -0700

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -260,10 +260,10 @@ class CarInterface(CarInterfaceBase):
       #tune.kiBP = [0., 5., 12., 20., 27.] stock
       #tune.kiV = [.35, .23, .20, .17, .1] stock
       if candidate in TSS2_CAR:
-        ret.vEgoStopping = 0.01  # car is near 0.1 to 0.2 when car starts requesting stopping accel
-        ret.vEgoStarting = 0.01  # needs to be > or == vEgoStopping
+        ret.vEgoStopping = 0.001  # car is near 0.1 to 0.2 when car starts requesting stopping accel
+        ret.vEgoStarting = 0.001  # needs to be > or == vEgoStopping
         ret.stopAccel = -0.40  # Toyota requests -0.4 when stopped
-        ret.stoppingDecelRate = 0.009  # reach stopping target smoothly - seems to take 0.5 seconds to go from 0 to -0.4
+        ret.stoppingDecelRate = 0.01  # reach stopping target smoothly - seems to take 0.5 seconds to go from 0 to -0.4
         #ret.longitudinalActuatorDelayLowerBound = 0.2
         #ret.longitudinalActuatorDelayUpperBound = 0.2
 
