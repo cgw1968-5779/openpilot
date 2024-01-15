bf4347da686df247867f4d404067440f88bd4bcf
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2024-01-15 11:06:59 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -253,12 +253,14 @@ class CarInterface(CarInterfaceBase):
     if candidate in TSS2_CAR or ret.enableGasInterceptor:
       tune.kpBP = [0., 5., 20.]
       tune.kpV = [1.3, 1.0, 0.7]
-      tune.kiBP = [0., 3.,  4.,  5.,   12., 20.,   24.,  40.]
-      tune.kiV = [.08, .16, .26, .22, .20, .168, .103, .006]
+      #tune.kiBP = [0., 3.,  4.,  5.,   12., 20.,   24.,  40.]
+      #tune.kiV = [.08, .16, .26, .22, .20, .168, .103, .006]
+      tune.kiBP = [0.,  5.,   12., 20.,   28.,  40.]
+      tune.kiV = [.35, .228, .20, .178, .10, .003]
       if candidate in TSS2_CAR:
-        #ret.vEgoStopping = 0.3  # car is near 0.1 to 0.2 when car starts requesting stopping accel
+        ret.vEgoStopping = 0.1  # car is near 0.1 to 0.2 when car starts requesting stopping accel
         ret.vEgoStarting = 0.1 # needs to be > or == vEgoStopping
-        #ret.stopAccel = -0.1  # Toyota requests -0.4 when stopped
+        ret.stopAccel = -0.4  # Toyota requests -0.4 when stopped
         ret.stoppingDecelRate = 0.01  # reach stopping target smoothly - seems to take 0.5 seconds to go from 0 to -0.4
         #ret.longitudinalActuatorDelayLowerBound = 0.3
         #ret.longitudinalActuatorDelayUpperBound = 0.3
