17e53743bef4922551df49822746ca01a851fe6c
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2024-03-14 00:17:04 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -58,7 +58,7 @@ STOP_DISTANCE = 6.0
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 1.0
+    return 2.0
   elif personality==log.LongitudinalPersonality.standard:
     return 1.0
   elif personality==log.LongitudinalPersonality.aggressive:
@@ -80,13 +80,13 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
 def get_dynamic_follow(v_ego, personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     x_vel =  [0.0,  3.0,  8.33,  13.90,  20,    25,    40]
-    y_dist = [1.2,  1.25, 1.40,  1.40,   1.50,  1.85,  2.0]
+    y_dist = [1.2,  1.25, 1.45,  1.45,   1.60,  1.85,  2.0]
   elif personality==log.LongitudinalPersonality.standard:
     x_vel =  [0.0,  3.0,  8.33,  13.90,  20,    25,    40]
-    y_dist = [1.00,  1.00, 1.20,  1.20,   1.25,  1.45,  1.5]
+    y_dist = [1.10, 1.10, 1.35,  1.35,   1.40,  1.45,  1.5]
   elif personality==log.LongitudinalPersonality.aggressive:
-    x_vel =  [0.0,  4.00, 8.33,  13.89,  20,    25,    40]
-    y_dist = [0.8,  0.80, 0.90,  0.90,   0.9,  1.105, 1.12]
+    x_vel =  [0.0,  13.89,  20,    25,    40]
+    y_dist = [0.95, 0.95,   1.00,  1.105, 1.12]
   else:
     raise NotImplementedError("Dynamic Follow personality not supported")
   return np.interp(v_ego, x_vel, y_dist)
