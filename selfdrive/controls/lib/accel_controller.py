77f327367a254e2dd890ce0c8cd649a94dab4f64
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2024-03-16 19:44:47 +0800

    Update accel_controller.py

--- selfdrive/controls/lib/accel_controller.py
+++ selfdrive/controls/lib/accel_controller.py
@@ -6,13 +6,13 @@ DP_ACCEL_NORMAL = 2
 DP_ACCEL_SPORT = 3
 
 # accel profile by @arne182 modified by cgw
-_DP_CRUISE_MIN_V =       [-0.12, -0.12, -0.17,  -0.65]
+_DP_CRUISE_MIN_V =       [-0.12,  -0.12,  -0.17,  -0.65]
 _DP_CRUISE_MIN_V_ECO =   [-0.10,  -0.10,  -0.15,  -0.60]
 _DP_CRUISE_MIN_V_SPORT = [-0.14,  -0.14,  -0.18,  -0.70]
-_DP_CRUISE_MIN_BP =      [0.,      2.,     5.,      42.]
+_DP_CRUISE_MIN_BP =      [0.,      2.,     5.,      36.]
 
 _DP_CRUISE_MAX_V =       [3.5, 3.5, 3.5, 3.2, 2.221, .863, .68,  .45, .35, .15]
-_DP_CRUISE_MAX_V_ECO =   [3.5, 3.5, 3.5, 2.8, 2.15,  .80,  .58,  .37, .322, .086]
+_DP_CRUISE_MAX_V_ECO =   [3.5, 3.5, 3.5, 2.8, 2.15,  .80,  .58,  .37, .322, .084]
 _DP_CRUISE_MAX_V_SPORT = [3.5, 3.5, 3.5, 3.5, 2.6,   1.3,  .82,  .65,  .45, .3]
 _DP_CRUISE_MAX_BP =      [0.,  1,   6.,  8.,  11.,   15.,  20.,  25., 30., 55.]
 
