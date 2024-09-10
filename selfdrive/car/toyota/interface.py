492d08cc18d2b10b057a6ed0d79f49ea6203d900
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2024-09-09 17:10:29 -0700

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -263,7 +263,7 @@ class CarInterface(CarInterfaceBase):
         ret.vEgoStopping = 0.01 # car is near 0.1 to 0.2 when car starts requesting stopping accel
         ret.vEgoStarting = 0.01  # needs to be > or == vEgoStopping
         ret.stopAccel = -0.40  # Toyota requests -0.4 when stopped
-        ret.stoppingDecelRate = 0.3  # reach stopping target smoothly - seems to take 0.5 seconds to go from 0 to -0.4
+        ret.stoppingDecelRate = 0.1  # reach stopping target smoothly - seems to take 0.5 seconds to go from 0 to -0.4
         #ret.longitudinalActuatorDelayLowerBound = 0.2
         #ret.longitudinalActuatorDelayUpperBound = 0.2
 
