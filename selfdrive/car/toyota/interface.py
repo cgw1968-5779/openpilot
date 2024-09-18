d62134594e1b2e995b5dd189e1b876a822932f10
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2024-09-18 14:16:46 -0700

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -252,7 +252,7 @@ class CarInterface(CarInterfaceBase):
     tune.deadzoneV = [.0, .15]
     if candidate in TSS2_CAR or ret.enableGasInterceptor:
       tune.kpBP = [0., 5., 20.]
-      tune.kpV = [1.6, 1.3, 1.0]
+      tune.kpV = [1.5, 1.3, 1.0]
       tune.kiBP = [0.,   5.,   12.,   20.,  27.,  40.]
       tune.kiV = [.33,   .23,  .20,  .17, .10, .06]
       #tune.kiBP = [0.,   2.,   3.,    4.,    5.,   12.,   20.,  27.,  40.]
